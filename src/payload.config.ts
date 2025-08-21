13dbf82f2a6b8cc8d6956019419eb376e4d67c04
Raghav <raghavendan2528@gmail.com>
2025-08-21 12:36:27 +0530

    loacalhost changes

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -25,7 +25,7 @@ export default buildConfig({
       baseDir: path.resolve(dirname),
     },
   },
-  cors: ['https://nomad-frontend-xi.vercel.app', 'http://localhost:3001'],
+  cors: ['https://nomad-frontend-xi.vercel.app', 'http://localhost:3000', 'http://localhost:3001'],
 
   collections: [Users, Media, Places, Header, Footer, Blogs, Pages, Forms],
   editor: lexicalEditor(),
