034f4130a4083feb9a4b87bc4a46e65a4328f3f7
Rahmah Building Services <rbs.backend2025@gmail.com>
2025-08-29 05:03:41 +0530

    Chore: Add comment to force new commit hash

--- src/app/(frontend)/blog/[slug]/page.tsx
+++ src/app/(frontend)/blog/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import BlogPostContent from './_components/BlogPostContent';
 
+// This component renders the blog post page based on the slug.
 export default function BlogPostPage({ params }: { params: { slug: string } }) {
   const { slug } = params;
   return <BlogPostContent slug={slug} />;
