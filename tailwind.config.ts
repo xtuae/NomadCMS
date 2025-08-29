0f3496eaa55951bc7c0a8a606dc2a99f52428e41
Rahmah Building Services <rbs.backend2025@gmail.com>
2025-08-29 09:43:42 +0530

    fix: update tailwind config content paths

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: [
     "./src/app/(frontend)/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/(frontend)/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
